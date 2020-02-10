0554cacce9d53a821f067863b5153bfc16e5ab69
carlossdf1 <carlos.sdf1@gmail.com>
2020-02-09 23:34:54 -0300

    Nuevas funcionalidades y limpieza del codigo

--- graf.js
+++ graf.js
@@ -1,33 +1,12 @@
 google.charts.load('current', {'packages':['corechart']});
-//google.charts.setOnLoadCallback(drawChart);
 
-function drawChart() {
+function drawChart(x) {
   console.log("DIBUJADO#####################################");
-  console.log(datos);
-  var data = google.visualization.arrayToDataTable(datos);
+  console.log(x);
+var data = google.visualization.arrayToDataTable(x);
 
-//var data = google.visualization.arrayToDataTable([
-//    ['Year', 'Sales', 'Expenses'],
-//    ['2004',  1000,      400],
-//    ['2005',  1170,      460],
-//    ['2006',  660,       1120],
-//    ['2007',  1030,      540]
-//  ]);
-
-
-//var data = google.visualization.arrayToDataTable([
-//  ["date time","2.5um","10 um"],
-//  ["11-23-2019 20-36-02",18,41],
-//  ["11-23-2019 20-35-21",29,53],
-//  ["11-23-2019 20-34-54",25,50],
-//  ["11-23-2019 20-34-27",35,51],
-//  ["11-23-2019 20-33-50",25,43],
-//  ["11-23-2019 20-33-19",20,47]
-//]);
-
-
-  var options = {
-    title: 'Company Performance',
+var options = {
+    title: 'Medicion de particulas en el Aire',
     curveType: 'function',
     legend: { position: 'bottom' }
   };
--- ordena.js
+++ ordena.js
@@ -1,66 +1,157 @@
-let vals=[];
-let datos=[];
+const vals=[];
 
 function leerArchivo(e) {
-    var archivos = e.target.files;
+    let archivos = e.target.files;
     if (!archivos) {
         return;
     }
 
-    datos.push(['Time','2.5um','10 um']);
+    for (let j=0,f; f = archivos[j] ; j++) {
 
-    console.log(archivos);
-
-    for (var j=0,f; f = archivos[j] ; j++) {
-        //console.log(f);
-        var lector = new FileReader();
+        let lector = new FileReader();
 
         lector.onload = (function(theFile) {
             return function(e){
-                let srt='';
-                let fila=[];
-                //console.log(e.target.result);
-                srt=(e.target.result).toString();
-                srt=srt.replace('MassCon  ug/m\r\n2.5um:', '');
-                srt=srt.replace('10 um:', '');
-                srt=srt.replace(/ /g, '');
-                srt=srt.replace('AT:', '');
-                srt=srt.replace('RH:', '\r\n');
-                srt=srt.replace('DP:', '');
-                srt=srt.replace('WB:', '\r\n');
-                srt=srt.replace(/\r\n/g, '-');
-                
-                var date = new Date(theFile.lastModified)
 
+                let srt=limpiaTxt((e.target.result).toString());
+             
+                let date = new Date(theFile.lastModified)
+                let fila=extraeDatos(srt);
                 //fila.push(theFile.name.replace('.txt', ''));
-                fila.push(date.getTime());
-
-                let num='';
-                for (var i=0; i<srt.length;i++){
-            
-                    if (srt[i]!='-'){
-                        num+=srt[i];
-                    }
-
-                    else{
-                        fila.push(num);
-                        num='';
-                    }
-                }
+                fila.unshift(date);
                 vals.push(fila);
-                datos.push([fila[0],parseInt(fila[1]),parseInt(fila[2])]);
-                
-                //console.log(datos);
-                //console.log(vals);
+
+                if(j+1==archivos.length){
+                    //window.location.href="#test6";
+
+                    console.log("PASO DATOS ############################################################");
+                    if(vals[0][0]!="Fecha"){
+                        vals.unshift(["Fecha","2,5um","10um","AT °C","RH %","DP °C","WB °C"]);
+                    }
+                    //console.log(vals);
+                    google.charts.setOnLoadCallback(drawChart(vals));
+                }
             }
         })(f);
         lector.readAsText(f);
     }//for
-    console.log("DATOS RECIEN PROCESADOS##########################################");
-    console.log(datos);
-    google.charts.setOnLoadCallback(drawChart);
 }
 
-document.getElementById('file-input')
-.addEventListener('change', leerArchivo, false);
+function limpiaTxt(srt){
+    srt=srt.replace('MassCon  ug/m\r\n2.5um:', '');
+    srt=srt.replace('10 um:', '');
+    srt=srt.replace(/ /g, '');
+    srt=srt.replace('AT:', '');
+    srt=srt.replace('RH:', '\r\n');
+    srt=srt.replace('DP:', '');
+    srt=srt.replace('WB:', '\r\n');
+    srt=srt.replace(/\r\n/g, '-');
+    return(srt);
+}
+
+function extraeDatos(srt){
+    let fila=[];
+
+    let num='';
+    for (var i=0; i<srt.length;i++){
+
+        if (srt[i]!='-'){
+            num+=srt[i];
+        }
+
+        else{
+            fila.push(parseInt(num));
+            num='';
+        }
+    }
+
+    return fila;
+}
+
+
+function switchVals(){
+    let graf=copiaVals();
+
+    let s=new Array(6);
+    
+    s[0]=document.getElementById("25").checked;
+    s[1]=document.getElementById("10").checked;
+    s[2]=document.getElementById("at").checked;
+    s[3]=document.getElementById("rh").checked;
+    s[4]=document.getElementById("dp").checked;
+    s[5]=document.getElementById("wb").checked;
+    
+    console.log(s);
+
+    let u=new Array(6);
+
+    if(s[0]==false){
+        u[0]=graf[0].indexOf("2,5um");
+        graf=borraColumnas(graf,u[0]);
+        //console.log(01,u[0]);
+    }
+    
+    if(s[1]==false){
+        u[1]=graf[0].indexOf("10um");
+        graf=borraColumnas(graf,u[1]);
+        //console.log(02,u[1]);
+    }
+
+    if(s[2]==false){
+        u[2]=graf[0].indexOf("AT °C");
+        graf=borraColumnas(graf,u[2]);
+        //console.log(03,u[2]);
+    }
+    if(s[3]==false){
+        u[3]=graf[0].indexOf("RH %");
+        graf=borraColumnas(graf,u[3]);
+        //console.log(04,u[3]);   
+    }
+    if(s[4]==false){
+        u[4]=graf[0].indexOf("DP °C");
+        graf=borraColumnas(graf,u[4]);
+        //console.log(05,u[4]);  
+    }
+    if(s[5]==false){
+        u[5]=graf[0].indexOf("WB °C");
+        graf=borraColumnas(graf,u[5]);
+        //console.log(06,u[5]);   
+    }
+    console.log(u);
+    return graf;
+}
+
+function dibujaGraf(){
+    let vals=switchVals();
+    google.charts.setOnLoadCallback(drawChart(vals));
+}
+
+function copiaVals(){
+    let datos=new Array();
+    for(let y=0;y<vals.length;y++){
+        //datos.push(vals[y]);
+        datos.push(new Array((vals[y][0]),(vals[y][1]),(vals[y][2]),(vals[y][3]),(vals[y][4]),(vals[y][5]),(vals[y][6])));
+    }
+    return datos;
+}
+
+
+function borraColumnas(dat,ind){
+    let datos=dat.slice();
+    for(let j=0;j<datos.length;j++){
+        datos[j].splice(ind,1);
+    }
+
+    return datos;
+}
+
+
+document.getElementById('file-input').addEventListener('change',leerArchivo, false);
+
+document.getElementById('25').addEventListener('change',dibujaGraf,false);
+document.getElementById('10').addEventListener('change',dibujaGraf,false);
+document.getElementById('at').addEventListener('change',dibujaGraf,false);
+document.getElementById('rh').addEventListener('change',dibujaGraf,false);
+document.getElementById('dp').addEventListener('change',dibujaGraf,false);
+document.getElementById('wb').addEventListener('change',dibujaGraf,false);
 
